fa23e351eb29eac0f626e6f0dad9e89963988f61
Richard Chang <chiel99@gmail.com>
2018-01-03 15:21:32 +0800

    Fix build break in Android
    Add missing lsmmc.c in Android.mk

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES:= mmc.c mmc_cmds.c
+LOCAL_SRC_FILES:= mmc.c mmc_cmds.c lsmmc.c
 LOCAL_SRC_FILES += 3rdparty/hmac_sha/sha2.c 3rdparty/hmac_sha/hmac_sha2.c
 LOCAL_MODULE := mmc_utils
 LOCAL_SHARED_LIBRARIES := libcutils libc
